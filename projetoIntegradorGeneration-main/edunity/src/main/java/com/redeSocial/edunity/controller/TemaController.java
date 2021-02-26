8620ec8658bb4d87a5bd7681de18d0acb83e87cf
Sergio-Duart <77842036+Sergio-Duart@users.noreply.github.com>
2021-02-26 16:08:10 -0300

    Revert "."

--- projetoIntegradorGeneration-main/edunity/src/main/java/com/redeSocial/edunity/controller/TemaController.java
+++ projetoIntegradorGeneration-main/edunity/src/main/java/com/redeSocial/edunity/controller/TemaController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,13 +51,4 @@ public class TemaController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
 	}
 	
-	@DeleteMapping ("/{id}")	
-	public void deleteTema (@PathVariable long id) {
-		temaRepository.deleteById(id);
-	}
-	
-	/*@DeleteMapping ("/{id}")
-	public ResponseEntity<?> deleteById (@PathVariable long id) {
-		return new ResponseEntity<String>("Tema deletado", HttpStatus.OK);
-	}*/
 }
